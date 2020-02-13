d4ca41ccc9b713b8147f5e639aa4254e73eaec29
Riley Slayden <slaydenriley@gmail.com>
2020-02-13 18:17:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,12 +1,13 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinzer.rb'
 
 class App < Sinatra::Base
   get '/user_input' do
     erb :user_input
   end
 
-  post '' do
-
+  post '/' do
+    
   end
 
 end
