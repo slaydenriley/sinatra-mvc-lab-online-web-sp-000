5b525bbbe19d77a470683de821c89df24103835f
Riley Slayden <slaydenriley@gmail.com>
2020-02-13 18:20:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,12 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
   get '/' do
     erb :user_input
   end
 
-  post '/' do
-    @words = PigLatinizer.new(params["string"])
+  post '/piglatinize' do
+    pl = PigLatinizer.new
+    @piglatin = pl.piglatinize(params[:user_phrase])
+    erb :results
   end
 
 end
